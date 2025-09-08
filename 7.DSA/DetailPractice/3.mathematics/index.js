7e0596ed1b0019801cbab9fe17c7295f0e484afa
Saurabh <saurabkumar679@gmail..com>
2025-09-08 07:16:31 +0530

    practice

--- 7.DSA/DetailPractice/3.mathematics/index.js
+++ 7.DSA/DetailPractice/3.mathematics/index.js
@@ -59,12 +59,69 @@ function isCompositeNumber(num) {
   return false;
 }
 
-console.log(isCompositeNumber(2));
+console.log(isCompositeNumber(4));
 console.log(isCompositeNumber(23));
 
 // Factorials of a number
 
+function factorial(num) {
+  if (typeof num !== "number") {
+    throw new Error("Input must be a number");
+  }
+
+  if (num < 1) {
+    return 0;
+  }
+
+  let result = 1;
+  let temp = num;
+  while (temp >= 1) {
+    result = result * temp;
+    temp--;
+  }
+  return `Factorial of ${num} is ${result}`;
+}
+
+console.log(factorial(5));
+
+//7654963001
+
 // Factors of a given number
+
+function factors(num) {
+  if (typeof num !== "number") {
+    throw new Error("Input must be a number");
+  }
+  const result = [];
+
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+console.log(factors(15));
+//T(n)= O(n)
+
+function factorsOptimized(n) {
+  if (typeof n !== "number" || n <= 0 || !Number.isInteger(n)) {
+    throw new Error("Input must be a positive integer");
+  }
+
+  const result = new Set(); // avoid duplicates (for perfect squares)
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      result.add(i);
+      result.add(n / i);
+    }
+  }
+  return Array.from(result).sort((a, b) => a - b);
+}
+console.log(factorsOptimized(15));
+
+//T(n)= (O(√n))
 
 // Armstrong number
 
